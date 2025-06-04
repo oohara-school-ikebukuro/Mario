e9ba9048b79a7224edc82fe0722c8dd82a9a87ab
shasu <l7momip1z6junin3y@outlook.jp>
2025-06-04 10:47:29 +0900

    helper

---
+++ Mario/scripts/helper/DxLibHelper.cpp
@@ -0,0 +1,64 @@
+﻿#include "./DxLibHelper.h"
+
+#include <DxLib.h>
+#include <algorithm>
+
+DxLibHelper::DxLibHelper(int fps) : fps(fps)
+{
+    if (fps <= 0) { // 0で割れないので
+        fps = std::clamp(fps, 1, 1000);
+    }
+    frameTime = 1000 / fps;
+    updateTime = GetNowCount();
+}
+
+int DxLibHelper::Init(WindowData window)
+{
+    switch (window.type)
+    {
+        case ScreenType::FULL_SCREEN:
+            ChangeWindowMode(TRUE);  // フルスクリーン
+            break;
+
+        case ScreenType::WINDOWED:
+        default:
+            ChangeWindowMode(TRUE);  // ウィンドウモード
+            SetGraphMode(window.width, window.height, 32);  // 解像度指定
+            break;
+    }
+
+    if (DxLib_Init() == -1) 
+    {
+        return -1; // 初期化失敗時に終了
+    }
+
+    SetDrawScreen(DX_SCREEN_BACK);  // ダブルバッファリング
+
+    return 0;
+}
+
+void DxLibHelper::RefreshScreen()
+{
+    // 描画処理
+    ScreenFlip();       // 画面更新
+    ProcessMessage();   // Windowsメッセージ処理
+    ClearDrawScreen();  // 画面クリア
+
+    // フレーム時間を計測
+    int now = GetNowCount(); // フレーム開始時間を取得
+    int deltaTime = now - updateTime;
+
+    // 必要なら待機して、更新時間
+    if (deltaTime < frameTime) {
+        WaitTimer(frameTime - deltaTime);
+        updateTime = GetNowCount();
+    }
+    else {
+        updateTime = now;
+    }
+}
+
+// DXLibの終了処理
+void DxLibHelper::Cleanup() {
+    DxLib_End();
+}
--- Mario/scripts/main.cpp
+++ Mario/scripts/main.cpp
@@ -3,7 +3,7 @@
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	if (DxLib_Init() == -1)	// ＤＸライブラリ初期化処理
 	{
 		return -1;			// エラーが起きたら直ちに終了
 	}
@@ -12,7 +12,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	WaitKey();				// キー入力待ち
 
-	DxLib_End();				// ＤＸライブラリ使用の終了処理
+	DxLib_End();			// ＤＸライブラリ使用の終了処理
 
 	return 0;				// ソフトの終了 
 }
+
+// ブロック崩しの、scripts/helperをコピー
+// Mario/scripts の中に、helperを貼り付け
+// ソリューションエクスプローラーで「既存の項目を追加」
+// helperの中の「DxLibHelper」を選択して追加
+
+// ※ main.cppが、scriptsの下に無いよ！！
+// ① エクスプローラー上で、main.cppをscriptsの下に移動しましょう
+// ② ソリューションエクスプローラーのmain.cppを一度削除しましょう
+// ③ ソリューションエクスプローラーで、既存の項目から、main.cppを追加
